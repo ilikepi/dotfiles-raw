be36ec5524132837ce2563bf3fac864bbec32f1f
James Blanding <ilikepi+github@gmail.com>
2021-12-04 13:48:34 -0500

    _template.user.js: update timestamp

--- usr/etc/firefox/_template.user.js
+++ usr/etc/firefox/_template.user.js
@@ -2,7 +2,7 @@
 //                                                                    //
 //  Firefox user.js template of globally applicable settings.         //
 //                                                                    //
-//  Updated: 2021-11-04 (arkenfox/user.js @ 17beb468)                 //
+//  Updated: 2021-12-04 (arkenfox/user.js @ fd860e6c)                 //
 //                                                                    //
 ////////////////////////////////////////////////////////////////////////
 
